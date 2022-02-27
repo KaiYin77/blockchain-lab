2ee12b23dfb413a873f55c068ddfbf6d080595de
Kevin Hong <kevin880208@gmail.com>
2022-02-27 17:06:57 +0800

    add lab1-Calculator test

--- lab1/test/TestCalculator.js
+++ lab1/test/TestCalculator.js
@@ -15,4 +15,23 @@ contract('Calculator', () => {
 		const currentResult = await contractInstance.getResult.call();
 		assert.equal(currentResult, 8, "result is not 8");
 	});
+	it('5 - 3 should be 2', async () => {
+		await contractInstance.sub(5, 3);  	
+		const currentResult = await contractInstance.getResult.call();
+		assert.equal(currentResult, 2, "result is not 8");
+	});
+	it('5 * 3 should be 15', async () => {
+		await contractInstance.mul(5, 3);  	
+		const currentResult = await contractInstance.getResult.call();
+		assert.equal(currentResult, 15, "result is not 8");
+	});
+	it('10 / 5 should be 2', async () => {
+		await contractInstance.div(10, 5);  	
+		const currentResult = await contractInstance.getResult.call();
+		assert.equal(currentResult, 2, "result is not 8");
+	});
+	it('10 / 0 should be ERROR', async () => {
+		await contractInstance.div(10, 0);  	
+		const currentResult = await contractInstance.getResult.call();
+	});
 });
